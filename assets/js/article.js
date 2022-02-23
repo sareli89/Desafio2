95f80440bfb17db29b767b77c450aa8ab919e116
Job <bojlk2@hotmail.com>
2022-02-23 16:48:53 -0600

    update

--- assets/js/article.js
+++ assets/js/article.js
@@ -1,24 +1,37 @@
 let idPost = location.search.slice(8)
-$.ajax({
-    url: `https://postsmedium-default-rtdb.firebaseio.com/posts/${idPost}.json`
-}).done((post) => {
-        let article = ''
-        article += `
+// $.ajax({
+//     url: `https://postsmedium-default-rtdb.firebaseio.com/posts/${idPost}.json`
+// }).done((post) => {
+//         let article = ''
+//         article += `
+fetch(`http://localhost:8080/articles/${idPost}`, {
+    method: 'GET',
+})
+.then(response => response.json())
+.then(json => {
+    const article = json.article
+    console.log(article)
+    
+    let template = ''
+    console.log(template)
+    article.forEach(article => {
+        // content.append(
+        let htmlTemplate = `
         <div class="card border-0 mb-3">
-                    <h1 class="mt-5 mb-3 title">${post.title}</h1>
+                    <h1 class="mt-5 mb-3 title">${article.title}</h1>
                     <div class="col col-md-8">
                         
                     </div>
                     
                     <div class="row g-0">
                         <div class="col-1">
-                            <img src="${post.urlImageAuthor}" width="40" height="40" class="img-fluid rounded-circle author-img" alt="Author">
+                            <img src="${article.urlImageAuthor}" width="40" height="40" class="img-fluid rounded-circle author-img" alt="Author">
                         </div>
                         
                         <div class="col-md-8">
                             <div class="card-body">
-                            <h5 class="card-title author">${post.author}</h5>
-                            <p class="card-text"><small class="text-muted date">${post.date}<small class="text-muted timetoread"> · ${post.timetoread}<i class="fas fa-star"></i></small></small></p>
+                            <h5 class="card-title author">${article.author}</h5>
+                            <p class="card-text"><small class="text-muted date">${article.date}<small class="text-muted timetoread"> · ${article.timetoread}<i class="fas fa-star"></i></small></small></p>
                             </div>
                             <div class="col d-md=flex d-md-justify-content-end">
                                 <i class="fab fa-twitter"></i>
@@ -32,16 +45,27 @@ $.ajax({
                 </div>
                 <div>
                     <div class="col mb-2">
-                        <img src="${post.urlImage}"  class="img-resposive w-100 mb-3" alt="">
+                        <img src="${article.urlImage}"  class="img-resposive w-100 mb-3" alt="">
                     </div>
                     
-                    <p class="mb-5 justify-text">${post.resume}</p>
+                    <p class="mb-5 justify-text">${article.resume}</p>
                 </div>
         `
+        template += htmlTemplate
+    })
+    
+
+    document.getElementById('card_article').innerHTML = template
+    
     $('#update_post').html(`<a href="update.html?idpost=${idPost}" class="text-white">Edit</a>`)
-    $('.title').html(`${post.title}`)
-    $('.card_article').html(article)
+    $('.title').html(`${article.title}`)
+    $('#card_article').html(article)
 })
+.catch(error => {
+    console.error('Get article error: ', error)
+})
+
+
 
 $('.back_button').click(() =>{
     location.replace('http://127.0.0.1:5500/')
--- assets/js/postindex.js
+++ assets/js/postindex.js
@@ -1,30 +1,39 @@
-$.get('https://postsmedium-default-rtdb.firebaseio.com/posts/.json', (listpost) => {
+// $.get('https://postsmedium-default-rtdb.firebaseio.com/posts/.json', (listpost) => {
+//     let template = ''
+//     var postArray = Object.keys(listpost).reverse()
+
+fetch('http://localhost:8080/articles', {
+    method: 'GET',
+})
+.then(response => response.json())
+.then(json => {
+    const articles = json.articles
+    console.log(articles)
+    // const content = document.getElementById('content')
     let template = ''
-    var postArray = Object.keys(listpost).reverse()
 
-    for(let post of postArray) {
-        let {title, author, resume, timetoread, date, tag, urlImageAuthor, urlImage} = listpost[post]
-
-        template += `
+    articles.forEach(article => {
+        // content.append(
+        let htmlTemplate = `
         <article class="card border-0">
             <div class="d-flex flex-row bd-highlight ms-3">
                 <div class="d-flex flex-row">
-                    <img width="20" height="20"src="${urlImageAuthor}" alt="Autor Image"    class="rounded-pill">
+                    <img width="20" height="20"src="${article.urlImageAuthor}" alt="Autor Image"    class="rounded-pill">
                 </div>
                 <div class="d-flex flex-row ms-2">
-                    <h6>${author}</h6>
+                    <h6>${article.author}</h6>
                 </div>
             </div>
             <div class="row flex-row">
             <div class="col-8">
                 <div class="card-body">
-                <a href="article.html?idpost=${post}" class="text-dark"><h2>${title}</h2></a>
-                <a href="article.html?idpost=${post}" class="text-dark"><p class="card-text overflow-auto text-muted d-none d-md-block">${resume.substring(0,110)}...</p></a>
+                <a href="article.html?idpost=${article._id}" class="text-dark"><h2>${article.title}</h2></a>
+                <a href="article.html?idpost=${article._id}" class="text-dark"><p class="card-text overflow-auto text-muted d-none d-md-block">${article.resume.substring(0,110)}...</p></a>
                 
                 <ul class="list-inline">                    
-                    <li class="list-inline-item text-muted">${date}</li>
-                    <li class="list-inline-item text-muted">${timetoread}</li>
-                    <li class="list-inline-item rounded-pill p-2 text-muted">${tag}</li>
+                    <li class="list-inline-item text-muted">${article.date}</li>
+                    <li class="list-inline-item text-muted">${article.timetoread}</li>
+                    <li class="list-inline-item rounded-pill p-2 text-muted">${article.tag}</li>
                     <li class="list-inline-item text-muted bg-">Selected For You.</li>
                     <button class="btn btn-outline-secondary border-0" style="background-color: white"> 
                         <img src="/images/SVG/bookmark_border_black_24dp.svg" alt="">
@@ -36,14 +45,25 @@ $.get('https://postsmedium-default-rtdb.firebaseio.com/posts/.json', (listpost) 
                 </div>
             </div>
             <div class="col-4 d-flex align-items-center">
-                <a href="article.html?idpost=${post}" style="background-image: url('${urlImage}');" class="img-post"></a>
+                <a href="article.html?idpost=${article._id}" style="background-image: url('${article.urlImage}');" class="img-post"></a>
             </div>
             </div>
         </article>
         `
-    }
-    $('.cards_list').html(template)
+        template += htmlTemplate
+    })
+
+    document.getElementById('content').innerHTML = template
 })
+.catch(error => {
+    console.error('Get posts error: ', error)
+})
+    // for(let post of postArray) {
+    //     let {title, author, resume, timetoread, date, tag, urlImageAuthor, urlImage} = listpost[post]
+
+//     }
+//     $('.cards_list').html(template)
+// })
 
 $('.back_button').click(() =>{
     location.replace('http://127.0.0.1:5500/')
